7e900670203e99fa0e38816bc4ea02175591c1c4
Simone DAgostino <simone-dagostino@rubygroupe.jp>
2019-12-13 15:22:40 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,34 +1,44 @@
 var path = require("path");
 var express = require("express");
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
 var pug = require("pug");
 
 var app = express();
+
+var credentials = {
+    key: fs.readFileSync("/etc/certificates/lsv2.machine.local/Encrypted.key.insecure"),
+    cert: fs.readFileSync("/etc/certificates/lsv2.machine.local/Encrypted.crt")
+};
+
 var sitePath = "../public_html";
-var port = 8080;
+
+var portHttp = 8080;
+var portHttps = 8443;
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 var tf = require("./tf");
 
 app.set("views", path.join(__dirname, sitePath + "/templates"));
 app.set("view engine", "pug");
 
-app.use(function(req, res, next) {
-    //console.log(`${new Date()} - ${req.method} request for ${req.url}`);
-    
-    next();
-});
-
 app.use(express.static(sitePath));
-
-app.listen(port, function() {
-    console.log(`Listen on ${port}`);
-});
 
 app.get("/", function(req, res) {
     res.render("index.pug");
 });
-
 app.get("/karada_sokutei", function(req, res) {
     tf.run(function(response) {
         res.render("karada_sokutei.pug", {'response': response, 'elements': JSON.stringify(response.elements)});
     });
+});
+
+httpServer.listen(portHttp, function() {
+    console.log(`Listen on ${portHttp}`);
+});
+httpsServer.listen(portHttps, function() {
+    console.log(`Listen on ${portHttps}`);
 });
